d00d66e0425eba55318ab2828acc3a1871d45c44
Book <1778934046@qq.com>
2024-05-28 09:59:12 +0800

    hot-second commit

--- src/main/java/org/xjs/Main.java
+++ src/main/java/org/xjs/Main.java
@@ -7,6 +7,8 @@ package org.xjs;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello master!");
         System.out.println("Hello hot-fix!");
     }
 }
